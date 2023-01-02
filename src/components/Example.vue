76d4bb1560f22dbf3338eafe17746bee5ede4750
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-01-02 01:53:23 +0100

    stable functionality for colorpicker

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
+import { colorBus } from "../myriad";
+import { ref, reactive, onMounted, watch } from 'vue'
 import { useColorName } from "../composables/useColorName"
 //import { eyeDropper } from "../composables/eyedropper"
+import { useMousePressed } from '@vueuse/core'
 
 const colorCanvas = ref<HTMLCanvasElement>()
 const hueCanvas = ref<HTMLCanvasElement>()
+const pallet = ref<HTMLCanvasElement>()
 
 function addHueSpectrum(ctx: CanvasRenderingContext2D, sizes: sizesType) {
   const {height, width} = sizes
@@ -111,14 +114,77 @@ function hueSlider(canvas?: HTMLCanvasElement) {
 
 const color = reactive({
   name: 'red',
-  value: '#ff0000',
+  hex: '#ff0000',
 })
 
 function assignColor(hex: string) {
   const get = useColorName(hex)
   const { name, value } = get()
   color.name = name
-  color.value = value
+  color.hex = value
+  colorBus({
+    background: color.hex,
+    foreground: '#ff5555',
+  }, pallet.value)
+}
+
+function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
+  const rect = canvas.getBoundingClientRect()
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  }
+}
+
+function rgbToHex(orig: any) {
+  var rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
+    hex = rgb ?
+    (rgb[1] | 1 << 8).toString(16).slice(1) +
+    (rgb[2] | 1 << 8).toString(16).slice(1) +
+    (rgb[3] | 1 << 8).toString(16).slice(1) : orig;
+  return "#" + hex;
+}
+
+function getPixelColor(evt: MouseEvent, canvas?: HTMLCanvasElement) {
+  if(!canvas) return
+  const ctx = canvas.getContext("2d", { willReadFrequently: true })
+  if(ctx === null) return
+  const {x, y} = getMousePos(canvas, evt)
+  const pixel = ctx.getImageData(x, y, 1, 1)
+  const data = pixel.data
+  const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3]})`
+  return rgbToHex(rgba)
+}
+
+let mousedown = ref(false)
+let activeCanvas = ref<EventTarget | null>(null)
+
+const { pressed } = useMousePressed()
+watch(pressed, (value: boolean) => {
+  if(value) return
+  activeCanvas.value = null
+  mousedown.value = value
+})
+
+function canvasCheck(e: MouseEvent, click: boolean) {
+  const returnCondition = !mousedown.value && !click
+  if(!mousedown.value) activeCanvas.value = e.target
+  if(activeCanvas.value === null) activeCanvas.value = e.target
+  return returnCondition
+}
+
+function spectrumChange(e: MouseEvent, click = false) {
+  if(canvasCheck(e, click)) return
+  if(activeCanvas.value !== e.target) return
+  const hex = getPixelColor(e, colorCanvas.value)
+  hex && assignColor(hex)
+}
+
+function hueChange(e: MouseEvent, click = false) {
+  if(canvasCheck(e, click)) return
+  if(activeCanvas.value !== e.target) return
+  const hex = getPixelColor(e, hueCanvas.value)
+  hex && colorWheel(colorCanvas.value, {hue: hex})
 }
 
 onMounted(() => {
@@ -126,42 +192,60 @@ onMounted(() => {
   hueSlider(hueCanvas.value)
   assignColor('#f1c1d1')
 })
-
 </script>
 
 <template>
-<div class="color">
-  <h2>{{ color.name }}</h2>
-  <p>{{ color.value }}</p>
+<div class="color" ref="pallet">
+  <p>{{ color.hex }}</p>
+  <h3>{{ color.name }}</h3>
 </div>
-<div class="canvas">
-  <canvas 
-    ref="colorCanvas"
-    width="300" 
-    height="300"
-  >
-  </canvas>
-  <canvas
-    ref="hueCanvas"
-    width="50" 
-    height="300">
-  </canvas>
+<div 
+  class="canvas" 
+  @mousedown="() => mousedown = true"
+>
+  <div class="colorCanvas">
+    <canvas
+      ref="colorCanvas"
+      width="300"
+      height="300"
+      @mousedown="(e) => spectrumChange(e, true)"
+      @mousemove="(e) => spectrumChange(e)"
+    >
+    </canvas>
+  </div>
+
+  <div class="hueCanvas">
+    <canvas
+      ref="hueCanvas"
+      width="50" 
+      height="300"
+      @mousedown="(e) => hueChange(e, true)"
+      @mousemove="(e) => hueChange(e)"
+    >
+    </canvas>
+  </div>
 </div>
 </template>
 
 <style lang="scss">
 .color {
-  background: var(--link);
+  background: v-bind("color.hex");
+  color: var(--foreground);
   width: 350px;
-  height: 50px;
+  height: 100px;
+  padding: 15px;
+  user-select: none;
+  * {
+    margin: 0px;
+    line-height: 1;
+  }
 }
 
 .canvas {
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: center; 
   width: max-content;
 }
 </style>
-
